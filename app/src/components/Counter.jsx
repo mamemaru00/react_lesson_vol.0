5078ae220b76cf815c7309ae022fa8ad5c7cedf6
mamemaru00 <k.shouhei0711@gmail.com>
2023-08-13 21:55:34 +0900

    tora08

--- app/src/components/Counter.jsx
+++ app/src/components/Counter.jsx
@@ -1,21 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Counter = () => {
     const [count, setCount] = useState(0)
 
     const countUp = () => {
-        setTimeout(() => setCount(prevState => prevState + 1), 1000)
+        setCount(prevState => prevState + 1)
     }
 
     const countDown = () => {
         setCount(prevState => prevState - 1)
     }
+
+    useEffect(() => {
+        console.log("Current count is...", count)
+    })
+
     return (
         <div>
-            <p>
-                良いカウンター<br />
-                現在のカウント数: {count}
-            </p>
+            <p>現在のカウント数: {count}</p>
             <button onClick={countUp}>up</button>
             <button onClick={countDown}>down</button>
         </div>
