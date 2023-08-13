261da132147fd5e97439a216a78350b3abf0253d
mamemaru00 <k.shouhei0711@gmail.com>
2023-08-13 17:28:26 +0900

    tora06

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,11 @@
 import Article from './components/Article';
-import BadArticle from './components/BadArticle';
 
 function App() {
   return (
     <div>
       <Article
-        title={'reactの入門勉強編5'}
-        content={'今日のトピックuseStateについて'}
-      />
-      <BadArticle
         title={'reactの入門勉強編6'}
-        content={'アイコンとタイトルを一緒に表示する'}
+        content={'今日のトピックuseStateについて'}
       />
     </div>
   );
--- app/src/components/Article.jsx
+++ app/src/components/Article.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import { Content, Title } from "./index";
+import PublishButton from "./PublishButton";
 
 //default import(名前なしimport)
 const Article = (props) => {
+    const [isPublished, setIsPublished] = useState(false)
+    // console.log(isPublished)
+    const publishArticle = () => {
+        setIsPublished(true)
+    }
     return (
         <div>
             <Title title={props.title} />
             <Content content={props.content} />
+            <PublishButton isPublished={isPublished} onClick={publishArticle} />
         </div>
     );
 };
